cd4bb0a1b00a800e4b2c21c5bcdd314d2c69986e
dennis <denduplon31@gmail.com>
2026-03-30 20:26:07 +0800

    image: add AI generated images; set: zambales.png to be hero image for the meantime

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,27 +1,38 @@
 import { Home, Search } from 'lucide-react';
 import Link from 'next/link';
+import Image from 'next/image';
+import Hero_Illustration from '@/public/zambales.png';
 
 const Hero = () => {
   return (
     <section className='hero relative min-h-[88vh] flex items-center border-b border-(--bg)'>
       <div className='hero-bg'></div>
       <div className='hero-grid-overlay'></div>
-      <div className='container relative z-2'>
-        <div className='hero-caption'>Zambales, Philippines</div>
-        <h1 className='w-1/2'>
-          Sun, <em>Surf</em> & Stays in Zambales
-        </h1>
-        <p className='text-muted'>
-          Browse and book the best resorts, cottages, and hotels along the
-          Zambales coast.
-        </p>
-        <div className='hero-cta mt-8 flex gap-4'>
-          <Link href={'/listings'} className='btn-primary'>
-            <Search size={16} /> Browse Listings
-          </Link>
-          <Link href={'/host'} className='btn-outline'>
-            <Home size={16} /> List Your Place
-          </Link>
+      <div className='container flex gap-2 items-center relative z-2'>
+        <div className='flex-1'>
+          <div className='hero-caption'>Zambales, Philippines</div>
+          <h1>
+            Sun, <em>Surf</em> & Stays in Zambales
+          </h1>
+          <p className='text-muted'>
+            Browse and book the best resorts, cottages, and hotels along the
+            Zambales coast.
+          </p>
+          <div className='hero-cta mt-8 flex gap-4'>
+            <Link href={'/listings'} className='btn-primary'>
+              <Search size={16} /> Browse Listings
+            </Link>
+            <Link href={'/host'} className='btn-outline'>
+              <Home size={16} /> List Your Place
+            </Link>
+          </div>
+        </div>
+        <div className='flex-1'>
+          <Image
+            src={Hero_Illustration}
+            className='rounded-2xl'
+            alt='book zambales hero illustration'
+          />
         </div>
       </div>
     </section>
